1ebcbfa8e177320b0acaeeb2ce101780104da102
MrBr <lbracano@gmail.com>
2016-09-14 19:11:48 +0200

    Improve DropDownMenu autoselect

--- components/DropDownMenu.js
+++ components/DropDownMenu.js
@@ -70,14 +70,20 @@ class DropDownMenu extends Component {
   }
 
   componentWillMount() {
-    this.autoSelect(this.props.options, this.props.selectedOption);
+    this.autoSelect(this.props.options);
     this.scrollDriver = new ScrollDriver();
     this.timingDriver = new TimingDriver();
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!this.state.selectedOption && _.size(nextProps.options) > 0) {
-      this.autoSelect(nextProps.options, nextProps.selectedOption);
+    const { selectedOption } = this.state;
+    if (selectedOption !== nextProps.selectedOption && nextProps.selectedOption) {
+      // Select the option when the selectedOption prop changes
+      this.selectOption(nextProps.selectedOption);
+    } else if (!selectedOption || nextProps.options !== this.props.options) {
+      // Automatically select the first option if there is no selected option
+      // or when options change
+      this.autoSelect(nextProps.options);
     }
   }
 
@@ -104,12 +110,16 @@ class DropDownMenu extends Component {
     return this.state.selectedOption;
   }
 
+  selectOption(option) {
+    this.setState({ selectedOption: option }, this.emitOnOptionSelectedEvent);
+  }
+
   /**
-   * Selects first item as default if non is selected
+   * Selects the first option by default.
    */
-  autoSelect(options = [], selectedOption) {
-    if (!selectedOption && !this.state.selectedOption && _.size(options) > 0) {
-      this.setState({ selectedOption: options[0] }, this.emitOnOptionSelectedEvent);
+  autoSelect(options = []) {
+    if (_.size(options) > 0) {
+      this.selectOption(options[0]);
     }
   }
 
@@ -169,7 +179,7 @@ class DropDownMenu extends Component {
     const fadeInEnd = optionPosition - (visibleOptions - 1.5) * optionHeight;
     const onPress = () => {
       this.close();
-      this.setState({ selectedOption: option }, this.emitOnOptionSelectedEvent);
+      this.selectOption(option);
     };
     return (
       <TouchableOpacity onPress={onPress} style={style.modalItem} onLayout={this.onOptionLayout}>
