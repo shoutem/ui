6e74784b615f939f50c146f70a18c42b29970282
Vladimir Adamic <vladimir@shoutem.com>
2016-09-16 10:11:25 +0200

    Use a WebView for displaying all videos
    - display video urls which are not embedded in a video player within a
      HTML video element
    - change Video source prop format
    - prevent scroll in WebView
    - remove NativeVideo component
    - remove react-native-video as a dependency
    - move WebViewVideo functionality to the Video component
    - use https for embedded video url transformations
    - add Videos example

--- components/Video/Video.js
+++ components/Video/Video.js
@@ -4,26 +4,49 @@ import React, {
 
 import {
   View,
+  WebView,
 } from 'react-native';
 
 import { connectStyle } from '@shoutem/theme';
-
-import { NativeVideo } from './NativeVideo';
-import { WebViewVideo } from './WebViewVideo';
+import { connectAnimation } from '@shoutem/animation';
 import VideoSourceReader from './VideoSourceReader';
 
 const propTypes = {
   width: PropTypes.number,
   height: PropTypes.number,
-  source: PropTypes.string,
+  source: PropTypes.shape({
+    uri: PropTypes.string,
+  }),
   style: PropTypes.object,
 };
+
+function createSourceObject(source) {
+  const sourceReader = new VideoSourceReader(source.uri);
+  const url = sourceReader.getUrl();
+
+  if (sourceReader.isEmbeddableVideo()) {
+    return {
+      uri: url,
+    };
+  }
+
+  const HTML = `
+    <video width="100%" height="auto" controls  >
+       <source src="${url}" >
+     </video>
+  `;
+
+  return {
+    html: HTML,
+  };
+}
 
 /**
  * Renders a Video based on the source type
  * if source is an url to a web player the
  * it is displayed in a WebView, if not
- * a native video player plays the video.
+ * a Video HTML element is displayed in the
+ * WebView.
  *
  * @returns {*}
  */
@@ -33,19 +56,12 @@ function Video({
   source,
   style,
 }) {
-  const sourceReader = new VideoSourceReader(source);
-  let VideoElement = NativeVideo;
-
-  if (sourceReader.isWebVideo()) {
-    VideoElement = WebViewVideo;
-  }
-
   return (
-    <View style={[style.container, { width, height }]}>
-      <VideoElement
-        source={{ uri: sourceReader.getUrl() }}
-        width={width}
-        height={height}
+    <View style={style.container}>
+      <WebView
+        style={{ width, height }}
+        source={createSourceObject(source)}
+        scrollEnabled={false}
       />
     </View>
   );
@@ -53,7 +69,8 @@ function Video({
 
 Video.propTypes = propTypes;
 
-const StyledVideo = connectStyle('shoutem.ui.Video', {})(Video);
+const AnimatedVideo = connectAnimation(Video);
+const StyledVideo = connectStyle('shoutem.ui.Video', {})(AnimatedVideo);
 
 export {
   StyledVideo as Video,
