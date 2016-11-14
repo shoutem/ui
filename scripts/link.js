09cfd71e1ddf0127f3fa6453ac260690c92597f2
Ivan Vuković <3.14wee@gmail.com>
2016-11-14 10:43:03 +0100

    Add postlink command

---
+++ scripts/link.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const exec = require('child_process').exec;
+
+// TODO (Ivan): Read native dependencies from package.json
+const nativeDependencies = [
+  'react-native-vector-icons',
+  'react-native-share',
+  'react-native-maps'
+];
+
+const linkCommands = nativeDependencies.map(dependency => `react-native link ${dependency}`);
+
+console.log('[@shoutem/ui] - linking native dependencies');
+
+exec(linkCommands.join(' && '), function(error, stdout, stderr) {
+  console.log(stdout);
+  console.log(stderr);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+  console.log('[@shoutem/ui] - native dependencies linked');
+});
