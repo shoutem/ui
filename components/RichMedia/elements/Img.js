6a72d9784cbe5a4e11942ea2cbcc41fd4ba58684
MrBr <lbracano@gmail.com>
2017-04-27 09:38:45 +0200

    Fix Img

--- components/RichMedia/elements/Img.js
+++ components/RichMedia/elements/Img.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import { ElementPropTypes } from '../components/RichMedia';
 import RichMediaImage from '../components/RichMediaImage';
 import { mapComponentProps, mapElementProps } from '../components/RichMedia';
 import { connectStyle } from '@shoutem/theme';
 
-function Img({ src }) {
+function Img({ src, style }) {
   const source = { uri: src };
 
   return (
     <RichMediaImage
       source={source}
-      style={{ resizeMode: 'contain', alignSelf: 'center' }}
+      style={style}
     />
   );
 }
 
 Img.propTypes = {
   ...RichMediaImage.propTypes,
+  ...ElementPropTypes,
 };
 
 export default connectStyle('shoutem.ui.RichMedia.img')(mapComponentProps(mapElementProps)(Img));
