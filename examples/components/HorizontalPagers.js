dbea5a1ee81f67c90aade13eee03dddc5856f22b
Željko Rumenjak <zeljko@shoutem.com>
2017-05-30 19:41:18 +0200

    Fix issues with gallery examples

--- examples/components/HorizontalPagers.js
+++ examples/components/HorizontalPagers.js
@@ -1,42 +1,46 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 
 import { Stage } from './Stage';
 import {
   View,
   HorizontalPager,
   Tile,
-  Icon,
   Image,
   Subtitle,
   Caption,
 } from '../../index';
 
+const window = Dimensions.get('window');
+
 export function HorizontalPagers() {
   return (
     <View styleName="vertical collapsible">
       <Stage title="Horizontal Pager (with tiles as content)">
-        <HorizontalPager
-          data={[
-            { description: 'description', title: 'When The Morning Dawns - DJ Silver Sample Album', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-1.png' }},
-            { description: 'description', title: 'When The Morning Dawns', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-2.png' }},
-            { description: 'description', title: 'DJ Silver Sample Album', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-3.png' }},
-            { description: 'description', title: 'When The Morning Dawns - DJ Silver Sample Album', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-4.png' }},
-          ]}
-          renderPage={(page) => {
-            return (
-              <Tile styleName="clear">
+        <View style={{ height: 250, width: window.width }}>
+          <HorizontalPager
+            data={[
+              { description: 'description', title: 'When The Morning Dawns - DJ Silver Sample Album', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-1.png' }},
+              { description: 'description', title: 'When The Morning Dawns', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-2.png' }},
+              { description: 'description', title: 'DJ Silver Sample Album', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-3.png' }},
+              { description: 'description', title: 'When The Morning Dawns - DJ Silver Sample Album', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-4.png' }},
+            ]}
+            renderPage={(page) => {
+              return (
                 <Image
                   styleName="large-wide"
                   source={{ uri: page.source.uri }}
                 >
-                  <Subtitle numberOfLines={2}>{page.title}</Subtitle>
-                  <Caption>{page.description}</Caption>
+                  <Tile>
+                    <Subtitle numberOfLines={2}>{page.title}</Subtitle>
+                    <Caption>{page.description}</Caption>
+                  </Tile>
                 </Image>
-              </Tile>
-            );
-          }}
-          pageMargin={20}
-        />
+              );
+            }}
+            pageMargin={20}
+          />
+        </View>
       </Stage>
     </View>
   );
--- examples/components/ImageGalleries.js
+++ examples/components/ImageGalleries.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dimensions } from 'react-native';
 
 import { Stage } from './Stage';
 import {
@@ -6,11 +7,13 @@ import {
   ImageGallery,
 } from '../../index';
 
+const window = Dimensions.get('window');
+
 export function ImageGalleries() {
   return (
     <View styleName="vertical collapsible">
       <Stage title="Image Gallery">
-        <View style={{ height: 500 }}>
+        <View style={{ height: 500, width: window.width }}>
           <ImageGallery
             data={[
               { description: 'description', title: 'title', source: { uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-1.png' }},
