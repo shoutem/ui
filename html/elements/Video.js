3dfa9fff47bba8c150cdb10284809b3fc0f27b78
MrBr <lbracano@gmail.com>
2017-06-30 15:52:32 +0200

    Keep Video thumbnail aspect ratio

--- html/elements/Video.js
+++ html/elements/Video.js
@@ -14,6 +14,7 @@ function Video({ src, thumbnail_url, style }) {
     <Image
       source={{ uri: thumbnail_url }}
       style={style.container}
+      keepRatio={false}
     >
       <View styleName="fill-parent">
         <UIVideo source={{ uri: src }} poster={thumbnail_url} />
