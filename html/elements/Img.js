c62e20125b9f3087a0b3d73099049b19ed60d3cc
MrBr <lbracano@gmail.com>
2017-06-30 15:50:52 +0200

    Implement isImg at Img element

--- html/elements/Img.js
+++ html/elements/Img.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import _ from 'lodash';
 
 import { ElementPropTypes } from '../Html';
 import Image from '../components/Image';
 import { combineMappers, mapElementProps } from '../Html';
+
+export function isImg(element) {
+  return _.get(element, 'tag') === 'img';
+}
 
 function Img({ src, style, zoomable }) {
   const source = { uri: src };
