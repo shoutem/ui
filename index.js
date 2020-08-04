bfb9b6612b6595be74d25d231994587fdebb24f2
Maja Klajić <maja.klajic@five.agency>
2020-08-04 10:00:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { setDefaultThemeStyle } from './init';
+import { setDefaultThemeStyle } from "./init";
 import getTheme, {
   defaultThemeVariables,
   dimensionRelativeToIphone,
   calculateLineHeight,
-} from './theme';
+} from "./theme";
 
 setDefaultThemeStyle();
 
@@ -16,69 +16,63 @@ export {
 };
 
 // Components
-export { ActionSheet } from './components/ActionSheet';
-export { View } from './components/View';
-export { Screen } from './components/Screen';
+export { ActionSheet } from "./components/ActionSheet";
+export { View } from "./components/View";
+export { Screen } from "./components/Screen";
 
-export { NavigationBar } from './components/NavigationBar';
-export { NavigationBarAnimations } from './components/NavigationBar/NavigationBarAnimations';
-export { DropDownMenu, DropDownModal } from './components/DropDownMenu';
-export { Overlay } from './components/Overlay';
+export { NavigationBar } from "./components/NavigationBar";
+export { NavigationBarAnimations } from "./components/NavigationBar/NavigationBarAnimations";
+export { DropDownMenu, DropDownModal } from "./components/DropDownMenu";
+export { Overlay } from "./components/Overlay";
 
-export { ScrollView } from './components/ScrollView';
-export { ListView } from './components/ListView';
-export { GridRow } from './components/GridRow';
+export { ScrollView } from "./components/ScrollView";
+export { ListView } from "./components/ListView";
+export { GridRow } from "./components/GridRow";
 
-export { TouchableOpacity } from './components/TouchableOpacity';
-export { TouchableNativeFeedback } from './components/TouchableNativeFeedback';
-export { Touchable } from './components/Touchable';
-export { Button } from './components/Button';
-export { Icon, createIcon } from './components/Icon';
+export { TouchableOpacity } from "./components/TouchableOpacity";
+export { TouchableNativeFeedback } from "./components/TouchableNativeFeedback";
+export { Touchable } from "./components/Touchable";
+export { Button } from "./components/Button";
+export { Icon, createIcon, IconSvg } from "./components/Icon";
 
-export { FormGroup } from './components/FormGroup';
-export { TextInput } from './components/TextInput';
+export { FormGroup } from "./components/FormGroup";
+export { TextInput } from "./components/TextInput";
 
-export { Spinner } from './components/Spinner';
-export { Switch } from './components/Switch';
+export { Spinner } from "./components/Spinner";
+export { Switch } from "./components/Switch";
 
-export { Video } from './components/Video';
-export { Image } from './components/Image';
-export { ImageBackground } from './components/ImageBackground';
-export { ImagePreview } from './components/ImagePreview';
-export { ImageGallery } from './components/ImageGallery';
-export { InlineGallery } from './components/InlineGallery';
-export { ImageGalleryOverlay } from './components/ImageGalleryOverlay';
-export { HorizontalPager } from './components/HorizontalPager';
-export { LoadingIndicator } from './components/LoadingIndicator';
-export { PageIndicators } from './components/PageIndicators';
-export { Html } from './html';
-export { SimpleHtml } from './html';
-export { ShareButton } from './components/ShareButton';
-export { LinearGradient } from './components/LinearGradient';
+export { Video } from "./components/Video";
+export { Image } from "./components/Image";
+export { ImageBackground } from "./components/ImageBackground";
+export { ImagePreview } from "./components/ImagePreview";
+export { ImageGallery } from "./components/ImageGallery";
+export { InlineGallery } from "./components/InlineGallery";
+export { ImageGalleryOverlay } from "./components/ImageGalleryOverlay";
+export { HorizontalPager } from "./components/HorizontalPager";
+export { LoadingIndicator } from "./components/LoadingIndicator";
+export { PageIndicators } from "./components/PageIndicators";
+export { Html } from "./html";
+export { SimpleHtml } from "./html";
+export { ShareButton } from "./components/ShareButton";
+export { LinearGradient } from "./components/LinearGradient";
 
-export {
-  Heading,
-  Title,
-  Subtitle,
-  Text,
-  Caption,
-} from './components/Text';
+export { Heading, Title, Subtitle, Text, Caption } from "./components/Text";
 
-export { Divider } from './components/Divider';
+export { Divider } from "./components/Divider";
 
-export { Card } from './components/Card';
-export { Row } from './components/Row';
-export { Tile } from './components/Tile';
+export { Card } from "./components/Card";
+export { Row } from "./components/Row";
+export { Tile } from "./components/Tile";
 
-export { Lightbox } from './components/Lightbox';
+export { Lightbox } from "./components/Lightbox";
 
-export { EmptyStateView } from './components/EmptyStateView';
-export { NumberInput } from './components/NumberInput';
-export { SearchField } from './components/SearchField';
+export { EmptyStateView } from "./components/EmptyStateView";
+export { NumberInput } from "./components/NumberInput";
+export { SearchField } from "./components/SearchField";
 
-export { Examples } from './examples/components';
+export { Examples } from "./examples/components";
 
-export { Device, Keyboard } from './helpers';
+export { Device, Keyboard } from "./helpers";
 
 // Constants
 export {
@@ -89,4 +83,4 @@ export {
   IPHONE_XR_NOTCH_PADDING,
   nativeDependencies,
   NAVIGATION_HEADER_HEIGHT,
-} from './const';
+} from "./const";
