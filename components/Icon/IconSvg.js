1bd21d671ad9aa238102549ec644e9eb3a34dee1
Maja Klajić <maja.klajic@five.agency>
2020-08-04 09:42:50 +0200

    Update IconSvg.js

--- components/Icon/IconSvg.js
+++ components/Icon/IconSvg.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { TouchableOpacity, StyleSheet } from 'react-native';
-import { gutter } from '~/modules/theme';
-import { Icons } from './assets';
-import { ButtonLabel } from './Texts';
+import React from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { gutter } from "~/modules/theme";
+import { Icons } from "./assets";
+import { ButtonLabel } from "./Texts";
 
-function IconSvg ({ iconName, iconProps, title, style, labelStyle, ...otherProps }) {
-    const Icon = Icons[iconName];
+function IconSvg({
+  iconName,
+  iconProps,
+  title,
+  style,
+  labelStyle,
+  ...otherProps
+}) {
+  const Icon = Icons[iconName];
 
-    if (!Icon) {
-      console.warn(
-        `Icon with ${iconName} name not found within the provided set`,
-      );
-
-      return null;
-    }
-
-    return (
-      <TouchableOpacity
-        style={[title && styles.flexButton, styles.button, style]}
-        hitSlop={{
-          top: gutter.large,
-          right: gutter.large,
-          bottom: gutter.large,
-          left: gutter.large,
-        }}
-        {...otherProps}>
-        <Icon {...iconProps} />
-        {title && <ButtonLabel labelStyle={labelStyle}>{title}</ButtonLabel>}
-      </TouchableOpacity>
+  if (!Icon) {
+    console.warn(
+      `Icon with ${iconName} name not found within the provided set`
     );
-  },
-);
+
+    return null;
+  }
+
+  return (
+    <TouchableOpacity
+      style={[title && styles.flexButton, styles.button, style]}
+      hitSlop={{
+        top: gutter.large,
+        right: gutter.large,
+        bottom: gutter.large,
+        left: gutter.large,
+      }}
+      {...otherProps}
+    >
+      <Icon {...iconProps} />
+      {title && <ButtonLabel labelStyle={labelStyle}>{title}</ButtonLabel>}
+    </TouchableOpacity>
+  );
+}
 
 const styles = StyleSheet.create({
   flexButton: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   button: {
     paddingVertical: gutter.regular,
