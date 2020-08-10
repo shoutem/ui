3442599d222aae61d11bb052a99da86efd9f7b82
Maja Klajić <maja.klajic@five.agency>
2020-08-10 10:14:02 +0200

    Update index.js

--- components/Icon/assets/index.js
+++ components/Icon/assets/index.js
@@ -1,5 +1,192 @@
+import about from "./about.svg";
+import activity from "./activity.svg";
+import addEvent from "./add-event.svg";
+import addFriend from "./add-friend.svg";
+import addToCart from "./add-to-cart.svg";
+import addToFavoritesOff from "./add-to-favorites-off.svg";
+import addToFavoritesOn from "./add-to-favorites-on.svg";
+import address from "./address.svg";
+import back from "./back.svg";
+import books from "./books.svg";
+import camSwitch from "./cam-switch.svg";
+import cart from "./cart.svg";
+import checkboxOff from "./checkbox-off.svg";
+import checkboxOn from "./checkbox-on.svg";
+import clearText from "./clear-text.svg";
+import close from "./close.svg";
+import comment from "./comment.svg";
+import deals from "./deals.svg";
+import directions from "./directions.svg";
+import downArrow from "./down-arrow.svg";
+import dropDown from "./drop-down.svg";
+import edit from "./edit.svg";
+import email from "./email.svg";
+import equalizer from "./equalizer.svg";
+import error from "./error.svg";
+import events from "./events.svg";
+import exitToApp from "./exit-to-app.svg";
+import eye from "./eye.svg";
+import eyeCrossed from "./eye-crossed.svg";
+import facebook from "./facebook.svg";
+import facebookLogo from "./facebook-logo.svg";
+import folder from "./folder.svg";
+import friends from "./friends.svg";
 import gift from "./gift.svg";
+import github from "./github.svg";
+import hangUp from "./hang-up.svg";
+import history from "./history.svg";
+import home from "./home.svg";
+import instagram from "./instagram.svg";
+import laptop from "./laptop.svg";
+import leftArrow from "./left-arrow.svg";
+import like from "./like.svg";
+import link from "./link.svg";
+import linkedin from "./linkedin.svg";
+import lock from "./lock.svg";
+import loyaltyCard from "./loyalty-card.svg";
+import maps from "./maps.svg";
+import micOff from "./mic-off.svg";
+import minusButton from "./minus-button.svg";
+import missing from "./missing.svg";
+import moreHorizontal from "./more-horizontal.svg";
+import musicVideo from "./music-video.svg";
+import myLocation from "./my-location.svg";
+import news from "./news.svg";
+import notifications from "./notifications.svg";
+import page from "./page.svg";
+import pause from "./pause.svg";
+import photo from "./photo.svg";
+import pin from "./pin.svg";
+import play from "./play.svg";
+import plusButton from "./plus-button.svg";
+import podcasts from "./podcasts.svg";
+import products from "./products.svg";
+import radio from "./radio.svg";
+import radiobuttonOff from "./radiobutton-off.svg";
+import radiobuttonOn from "./radiobutton-on.svg";
+import receipt from "./receipt.svg";
+import refresh from "./refresh.svg";
+import restaurant from "./restaurant.svg";
+import restaurantMenu from "./restaurant-menu.svg";
+import rightArrow from "./right-arrow.svg";
+import rssFeed from "./rss-feed.svg";
+import rsvp from "./rsvp.svg";
+import search from "./search.svg";
+import settings from "./settings.svg";
+import share from "./share.svg";
+import shareAndroid from "./share-android.svg";
+import sidebar from "./sidebar.svg";
+import socialWall from "./social-wall.svg";
+import stamp from "./stamp.svg";
+import stop from "./stop.svg";
+import takeAPhoto from "./take-a-photo.svg";
+import trophy from "./trophy.svg";
+import turnOff from "./turn-off.svg";
+import tweet from "./tweet.svg";
+import uber from "./uber.svg";
+import unfriend from "./unfriend.svg";
+import upArrow from "./up-arrow.svg";
+import userProfile from "./user-profile.svg";
+import users from "./users.svg";
+import videoCam from "./video-cam.svg";
+import videoCamOff from "./video-cam-off.svg";
+import videoChat from "./video-chat.svg";
+import web from "./web.svg";
 
 export const Icons = {
-  gift,
+  about: about,
+  activity: activity,
+  "add-event": addEvent,
+  "add-friend": addFriend,
+  "add-to-cart": addToCart,
+  "add-to-favorites-off": addToFavoritesOff,
+  addToFavoritesOn: addToFavoritesOn,
+  address: address,
+  back: back,
+  books: books,
+  call: call,
+  camSwitch: camSwitch,
+  cart: cart,
+  "checkbox-off": checkboxOff,
+  "checkbox-on": checkboxOn,
+  "clear-text": clearText,
+  close: close,
+  comment: comment,
+  deals: deals,
+  directions: directions,
+  "down-arrow": downArrow,
+  "drop-down": dropDown,
+  edit: edit,
+  email: email,
+  equalizer: equalizer,
+  error: error,
+  events: events,
+  "exit-to-app": exitToApp,
+  eye: eye,
+  "eye-crossed": eyeCrossed,
+  facebook: facebook,
+  "facebook-logo": facebookLogo,
+  folder: folder,
+  friends: friends,
+  gift: gift,
+  github: github,
+  "hang-up": hangUp,
+  history: history,
+  home: home,
+  instagram: instagram,
+  laptop: laptop,
+  "left-arrow": leftArrow,
+  like: like,
+  link: link,
+  linkedin: linkedin,
+  lock: lock,
+  "loyalty-card": loyaltyCard,
+  maps: maps,
+  "mic-off": micOff,
+  "minus-button": minusButton,
+  missing: missing,
+  "more-horizontal": moreHorizontal,
+  "music-video": musicVideo,
+  "my-location": myLocation,
+  news: news,
+  notifications: notifications,
+  page: page,
+  pause: pause,
+  photo: photo,
+  pin: pin,
+  play: play,
+  "plus-button": plusButton,
+  podcasts: podcasts,
+  products: products,
+  radio: radio,
+  "radiobutton-off": radiobuttonOff,
+  "radiobutton-on": radiobuttonOn,
+  receipt: receipt,
+  refresh: refresh,
+  restaurant: restaurant,
+  "restaurant-menu": restaurantMenu,
+  "right-arrow": rightArrow,
+  "rss-feed": rssFeed,
+  rsvp: rsvp,
+  search: search,
+  settings: settings,
+  share: share,
+  "share-android": shareAndroid,
+  sidebar: sidebar,
+  "social-wall": socialWall,
+  stamp: stamp,
+  stop: stop,
+  "take-a-photo": takeAPhoto,
+  trophy: trophy,
+  "turn-off": turnOff,
+  tweet: tweet,
+  uber: uber,
+  unfriend: unfriend,
+  "up-arrow": upArrow,
+  "user-profile": userProfile,
+  users: users,
+  "video-cam": videoCam,
+  "video-cam-off": videoCamOff,
+  "video-chat": videoChat,
+  web: web,
 };
