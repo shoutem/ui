124ca3a15eb180a1bd8e950620bf803c62b93f55
Maja Klajić <maja.klajic@five.agency>
2020-09-03 20:36:00 +0200

    Add default icon width and height

--- theme.js
+++ theme.js
@@ -1183,6 +1183,8 @@ export default (variables = defaultThemeVariables) => ({
 
     backgroundColor: 'transparent',
     color: variables.primaryButtonText.color,
+    width: 24,
+    height: 24,
   },
 
   'shoutem.ui.Spinner': {
