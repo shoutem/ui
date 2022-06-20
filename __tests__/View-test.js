c7276d50f4a4ef951209b42072fd53da6bb508dd
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2022-06-20 08:37:37 +0200

    Add snapshots with frequent View styleNames (#707)

--- __tests__/View-test.js
+++ __tests__/View-test.js
@@ -9,3 +9,47 @@ test('View renders correctly', () => {
 
   expect(tree).toMatchSnapshot();
 });
+
+test('View renders children vertically with vertical styleName', () => {
+  const tree = renderer.create(
+    <View styleName="vertical">
+      <View style={{ height: 40, width: 40, backgroundColor: 'red' }} />
+      <View style={{ height: 40, width: 40, backgroundColor: 'green' }} />
+    </View>
+  );
+});
+
+test('View centers children horizontally with h-center styleName', () => {
+  const tree = renderer.create(
+    <View style={{ width: 100 }} styleName="vertical h-center">
+      <View style={{ height: 40, width: 40, backgroundColor: 'red' }} />
+      <View style={{ height: 40, width: 40, backgroundColor: 'green' }} />
+    </View>
+  );
+});
+
+test('View renders children horizontally with horizontal styleName', () => {
+  const tree = renderer.create(
+    <View styleName="horizontal">
+      <View style={{ height: 40, width: 40, backgroundColor: 'red' }} />
+      <View style={{ height: 40, width: 40, backgroundColor: 'green' }} />
+    </View>
+  );
+});
+
+test('View centers children vertically with v-center styleName', () => {
+  const tree = renderer.create(
+    <View style={{ height: 100 }} styleName="horizontal v-center">
+      <View style={{ height: 40, width: 40, backgroundColor: 'red' }} />
+      <View style={{ height: 40, width: 40, backgroundColor: 'green' }} />
+    </View>
+  );
+});
+
+test('View fills parent with fill-parent styleName', () => {
+  const tree = renderer.create(
+    <View style={{ height: 40, width: 40 }}>
+      <View style={{ backgroundColor: 'red' }} styleName="fill-parent" />
+    </View>
+  );
+});
