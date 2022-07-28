16c70a3e1fa7cf14550244ce222d0659bfe7d2ce
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-07-28 10:40:29 +0200

    Update exports 🤦‍♀️
    Forgot to export all consts moved to device-selector yesterday.. I really hope we can rewrite and update this service entirely sometime soon..

--- helpers/index.js
+++ helpers/index.js
@@ -1,2 +1,13 @@
-export { Device } from './device-selector';
+export {
+  Device,
+  IPHONE_12_LONG_SIDE,
+  IPHONE_12_MAX_LONG_SIDE,
+  IPHONE_X_HOME_INDICATOR_PADDING,
+  IPHONE_X_LONG_SIDE,
+  IPHONE_X_NOTCH_PADDING,
+  IPHONE_XR_LONG_SIDE,
+  IPHONE_XR_NOTCH_PADDING,
+  NAVIGATION_BAR_HEIGHT,
+  NAVIGATION_HEADER_HEIGHT,
+} from './device-selector';
 export { calculateKeyboardOffset, default as Keyboard } from './keyboard';
