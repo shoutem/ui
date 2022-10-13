064841ea48ddf650c3644785ccd8ad00deb56dbe
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-10-13 12:28:35 +0200

    Change Lightbox export

--- components/Lightbox.js
+++ components/Lightbox.js
@@ -1,6 +1,15 @@
-import Lightbox from 'react-native-lightbox-v2';
+import React, { PureComponent } from 'react';
+import RNLightbox from 'react-native-lightbox-v2';
 import { connectAnimation } from '@shoutem/animation';
 import { connectStyle } from '@shoutem/theme';
+
+// Exporting as class because Animated.createAnimatedComponent
+// doesn't support stateless functional components
+class Lightbox extends PureComponent {
+  render() {
+    return <RNLightbox {...this.props} />;
+  }
+}
 
 const AnimatedLightbox = connectAnimation(Lightbox);
 const StyledLightbox = connectStyle('shoutem.ui.Lightbox')(AnimatedLightbox);
