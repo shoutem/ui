4a0584a503a9a8f9da69be79cec3ce5b89e54a96
Definitely Not Vlad <vlad@definitely-not-vlad.com>
2023-08-22 15:02:15 +0200

    Fix asset paths

--- scripts/link-assets.js
+++ scripts/link-assets.js
@@ -7,6 +7,22 @@ const execPath = path.resolve(
   'lib',
   'cli.js',
 );
-const execArgs = [execPath, '-a', ...allAssetPaths, '-n-u'];
+
+// Path is the relative path seen as with root app directory being the CWD.
+const assetPaths = [
+  './node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-Black.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-Light.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf',
+  './node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf',
+];
+
+const execArgs = [execPath, '-a', assetPaths, '-n-u'];
 
 execSync('node', execArgs);
