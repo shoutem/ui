72ecf1d8bdd080a588b203c2caf05567a00be927
tomislav-arambasic <tomislav.arambasic@gmail.com>
2024-07-16 16:00:22 +0200

    add immediate unsupported toast action if no callback is provided

--- services/unavailableInWeb.js
+++ services/unavailableInWeb.js
@@ -1,5 +1,6 @@
 import { Platform } from 'react-native';
 import { Toast } from '@shoutem/ui';
+import _ from 'lodash';
 
 const isWeb = Platform.OS === 'web';
 
@@ -7,6 +8,17 @@ export const unavailableInWeb = callback => {
   if (!isWeb) {
     return callback;
   }
+
+  // If callback is undefined, show toast immediatelly.
+  if (_.isUndefined(callback)) {
+    Toast.showInfo({
+      title: 'Feature currently unavailable',
+      message:
+        'This feature is currently unavailable in Web Preview. For better preview experience download Disclose app.',
+    });
+  }
+
+  // Otherwise, return callback to be executed on user action.
 
   return () =>
     Toast.showInfo({
