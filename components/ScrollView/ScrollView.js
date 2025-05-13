7b4f2729bdc0e036007be3bb9f0840e6c4d42fc7
sstimac <sstimac.contact@gmail.com>
2025-05-13 12:59:14 +0200

    adapt per animation and theme changes

--- components/ScrollView/ScrollView.js
+++ components/ScrollView/ScrollView.js
@@ -1,80 +1,56 @@
-import React, { PureComponent } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import { Animated } from 'react-native';
-import autoBindReact from 'auto-bind/react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
-import { ScrollDriver } from '@shoutem/animation';
+import { ScrollDriver, AnimationDriverContext } from '@shoutem/animation';
 import { connectStyle } from '@shoutem/theme';
-import {
-  ScrollDriverContext,
-  ScrollDriverProvider,
-} from './ScrollDriverProvider.js';
 
-class ScrollView extends PureComponent {
-  static DriverProvider = ScrollDriverProvider;
+const ScrollView = ({ driver, onScroll, primary, style, ...otherProps }) => {
+  const animationDriver = useRef(
+    driver ||
+    new ScrollDriver(
+      { useNativeDriver: true, nativeScrollEventThrottle: 20 },
+      onScroll,
+    )
+  );
+  const animationDriverContext = useContext(AnimationDriverContext);
 
-  static DriverContext = ScrollDriverContext;
+  // When transition to and back from the screen containing the scrollview,
+  // we want to switch to appropriate animation driver. We use the following hook
+  // to derive the new driver and cascading styles before the actual navigation transition is done
+  // to avoid rerendering when the actual nav transition completes
+  useFocusEffect(() => {
+    const { animationDriver: prevAnimationDriver, setAnimationDriver } = animationDriverContext;
 
-  constructor(props, context) {
-    super(props, context);
-
-    autoBindReact(this);
-
-    this.animationDriver =
-      props.driver ||
-      new ScrollDriver(
-        { useNativeDriver: true, nativeScrollEventThrottle: 20 },
-        props.onScroll,
-      );
-  }
-
-  componentDidMount() {
-    const { driverProvider } = this.context;
-    const { primary } = this.props;
-
-    if (driverProvider) {
-      driverProvider.setAnimationDriver(this.animationDriver, primary);
+    if (setAnimationDriver) {
+      setAnimationDriver(animationDriver.current, primary);
     }
-  }
 
-  componentDidUpdate() {
-    const { driver } = this.props;
+    return () => {
+      if (prevAnimationDriver) {
+        setAnimationDriver(prevAnimationDriver)
+      }};
+  });
 
-    if (driver && this.animationDriver !== driver) {
-      this.animationDriver = driver;
+  useEffect(() => {
+    if (driver && animationDriver.current !== driver) {
+      animationDriver.current = driver;
     }
-  }
+  }, [driver]);
 
-  setWrappedInstance(component) {
-    this.wrappedInstance = component;
-  }
+  const { scrollViewProps } = animationDriver?.current;
+  const { contentContainerStyle, ...otherStyle } = style;
 
-  render() {
-    const { style, ...otherProps } = this.props;
-    const { driverProvider } = this.context;
-    const { scrollViewProps } = this.animationDriver;
-    const { contentContainerStyle, ...otherStyle } = style;
-
-    return (
-      <ScrollDriverContext.Provider
-        value={{
-          driverProvider,
-          animationDriver: this.animationDriver,
-        }}
-      >
-        <Animated.ScrollView
-          ref={this.setWrappedInstance}
-          contentContainerStyle={contentContainerStyle}
-          {...scrollViewProps}
-          {..._.omit(otherProps, 'onScroll')}
-          style={otherStyle}
-        />
-      </ScrollDriverContext.Provider>
-    );
-  }
+  return (
+      <Animated.ScrollView
+        contentContainerStyle={contentContainerStyle}
+        {...scrollViewProps}
+        {..._.omit(otherProps, 'onScroll')}
+        style={otherStyle}
+      />
+  );
 }
-
-ScrollView.contextType = ScrollDriverContext;
 
 ScrollView.propTypes = {
   ...Animated.ScrollView.propTypes,
@@ -86,28 +62,5 @@ ScrollView.defaultProps = {
 };
 
 const StyledScrollView = connectStyle('shoutem.ui.ScrollView')(ScrollView);
-
-function getRNScrollViewComponent(context) {
-  // wrappedInstance.wrappedInstance._component:
-  //   1st wrappedInstance -> StyledScrollView
-  //   2nd wrappedInstance -> ScrollView (Shoutem UI)
-  //   _component -> Animated.ScrollView
-  // more info about _component: https://stackoverflow.com/questions/42051368/scrollto-is-undefined-on-animated-scrollview
-  return _.get(context, 'wrappedInstance.wrappedInstance._component');
-}
-
-StyledScrollView.prototype.scrollTo = function scrollTo(coordinates) {
-  const scrollView = getRNScrollViewComponent(this);
-  if (scrollView) {
-    scrollView.scrollTo(coordinates);
-  }
-};
-
-StyledScrollView.prototype.scrollToEnd = function scrollToEnd(animation) {
-  const scrollView = getRNScrollViewComponent(this);
-  if (scrollView) {
-    scrollView.scrollToEnd(animation);
-  }
-};
 
 export { StyledScrollView as ScrollView };
