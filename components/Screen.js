29ecf5d834d88fd966c9f03de397bed077b3e943
Srđan Branković <62928970+s-brankovic@users.noreply.github.com>
2026-05-08 07:32:29 +0200

    Feat/screen safe area bottom inset (#925)
    * Add safe area bottom inset to the Screen component
    
    * Remove List view home indicator padding

--- components/Screen.js
+++ components/Screen.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { connectAnimation } from '@shoutem/animation';
 import { connectStyle } from '@shoutem/theme';
 
-const AnimatedScreen = connectAnimation(View);
+const SafeAreaAwareView = ({ style, ...rest }) => {
+  const insets = useSafeAreaInsets();
+  const safeAreaStyle = { paddingBottom: insets.bottom };
+
+  return <View {...rest} style={[style, safeAreaStyle]} />;
+};
+
+const AnimatedScreen = connectAnimation(SafeAreaAwareView);
 const Screen = connectStyle('shoutem.ui.Screen')(AnimatedScreen);
 
 export { Screen };
